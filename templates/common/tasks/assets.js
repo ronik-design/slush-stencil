38e780d3e7c799ecadfa9ce8e3f33c73e6f0b24a
Michael Shick <m@shick.us>
2015-11-24 16:53:48 -0500

    bump some dependencies

--- templates/common/tasks/assets.js
+++ templates/common/tasks/assets.js
@@ -1,6 +1,4 @@
-"use strict";
-
-var gulp = require("gulp");
+const gulp = require("gulp");
 var util = require("gulp-util");
 var plumber = require("gulp-plumber");
 var gulpIf = require("gulp-if");
